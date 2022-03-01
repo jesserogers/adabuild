e08bdee806f79f7fbeebd6ec9bcf8c4150c3f8e7
Jesse Rogers <jesse.rogers@adaptiva.com>
2022-03-01 09:48:24 -0800

    add state persistence logic to Monitor
    should probably just move all state code to the MonitorState class and let Monitor delegate it, but that can go in another commit

--- src/monitor/monitor.ts
+++ src/monitor/monitor.ts
@@ -3,6 +3,9 @@ import * as vscode from "vscode";
 import { ConfigurationService } from "../config";
 import { FileSystemService } from "../filesystem";
 import { WindowService } from "../window";
+import { debounce } from "../utils";
+import { MonitorState } from "./monitor-state";
+import { IMonitorState } from "./monitor-state.interface";
 
 /**
  * @author Jesse Rogers <jesse.rogers@adaptiva.com>
@@ -13,13 +16,21 @@ import { WindowService } from "../window";
 })
 export class Monitor implements OnDestroy {
 
-	private _changedLibraries = new Set<string>();
+	private static readonly STATE_FILE_NAME: string = ".adabuildstate";
 
-	private _history = new Map<string, number>();
+	private _changed: Set<string> = new Set();
+
+	private _history: Map<string, number> = new Map();
 
 	private _watcher!: vscode.FileSystemWatcher;
 
 	private _changeListener!: vscode.Disposable;
+
+	private _saveStateOnDebounce = debounce(this.saveState.bind(this), 1000);
+
+	private get _statePath(): string {
+		return this.fileSystem.root + "\\" + Monitor.STATE_FILE_NAME;
+	}
 
 	constructor(
 		@Inject(ConfigurationService) private config: ConfigurationService,
@@ -33,18 +44,8 @@ export class Monitor implements OnDestroy {
 		if (!this.config.buildConfig)
 			return;
 
-		if (this._watcher)
-			this._watcher.dispose();
-	
-		this._watcher = this.fileSystem.watch(this.config.buildConfig.projectsRootGlob);
-		this._changeListener = this._watcher.onDidChange(uri => {
-			this.config.buildConfig.projectDefinitions.forEach(_project => {
-				if (_project?.type === "library" && uri.path.includes(`/${_project.name}/`)) {
-					this.window.log(_project.name + " changed");
-					this._changedLibraries.add(_project.name);
-				}
-			});
-		});
+		this._fetchPreviousState();
+		this._watchProjects();
 	}
 
 	public reset(...projects: string[]): void {
@@ -54,23 +55,75 @@ export class Monitor implements OnDestroy {
 				this.window.log("Successfully reset " + project);
 			});
 		} else {
-			this._changedLibraries.clear();
+			this._changed.clear();
 			this._history.clear();
 			this.window.log("Successfully reset all projects");
 		}
+		this.saveState();
 	}
 
 	public hasChanged(library: string): boolean {
-		return this._changedLibraries.has(library) || !this._history.has(library);
+		return this._changed.has(library) || !this._history.has(library);
 	}
 
 	public registerChange(project: string): void {
-		this._changedLibraries.add(project);
+		this._changed.add(project);
 	}
 
 	public record(project: string): void {
 		const _previous: number | undefined = this._history.get(project);
 		this._history.set(project, (_previous || 0) + 1);
+		this._changed.delete(project);
+	}
+
+	public saveState(): void {
+		const _state: MonitorState = new MonitorState(this._changed, this._history);
+
+		this.window.log(`Saving state to ${Monitor.STATE_FILE_NAME}...`);
+		this.fileSystem.writeFile(this._statePath, _state.export());
+	}
+
+	private _watchProjects(): void {
+		if (this._watcher)
+			this._watcher.dispose();
+	
+		this._watcher = this.fileSystem.watch(this.config.buildConfig.projectsRootGlob);
+		this._changeListener = this._watcher.onDidChange(uri => {
+			this.config.buildConfig.projectDefinitions.forEach(_project => {
+				if (_project?.type === "library" && uri.path.includes(`/${_project.name}/`)) {
+					this.window.log(_project.name + " changed");
+					this._changed.add(_project.name);
+
+					this._saveStateOnDebounce();
+				}
+			});
+		});
+	}
+
+	private _fetchPreviousState(): void {
+		this.fileSystem.getJsonFile<IMonitorState>(this._statePath).then(_state => {
+			if (!_state)
+				return;
+
+			this.window.log("Found existing state file.");
+
+			try {
+				if (_state.changed)
+					_state.changed.forEach(_project => this._changed.add(_project));
+
+				if (_state.history)
+					for (const _project in _state.history)
+						if (_state.history[_project])
+							this._history.set(_project, _state.history[_project]);
+
+			} catch (_err) {
+				console.error(_err);
+				this.window.error("Failed to parse state file.");
+			}
+
+		}).catch(_error => {
+			this.window.log(_error);
+		});
 	}
 
 	onDestroy(): void {
