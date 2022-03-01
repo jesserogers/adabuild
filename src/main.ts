d8d6543b1dc97448529d7e6d9b754ed920439b79
Jesse Rogers <jesse.rogers@adaptiva.com>
2022-03-01 09:49:25 -0800

    add TextEncoder provider

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { destroyAllInstances, inject } from "@kuroi/syringe";
-import { TextDecoder } from "util";
+import { TextDecoder, TextEncoder } from "util";
 import { ExtensionContext } from 'vscode';
 import { AdaBuildExtension } from "./app";
 
 export function activate(context: ExtensionContext) {
 	try {
 		const adabuild: AdaBuildExtension = inject(AdaBuildExtension, {
-			providers: [ TextDecoder ]
+			providers: [
+				TextDecoder,
+				TextEncoder
+			]
 		});
 		context.subscriptions.push(...adabuild.generateCommands());
 	} catch (_err) {
