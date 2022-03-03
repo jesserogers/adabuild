bc6d6c8ffd330d6acabf19695b7de2f3e3aba1ca
Jesse Rogers <jesse.rogers@adaptiva.com>
2022-03-03 12:30:25 -0800

    Update thread.ts

--- src/common/cmd/thread.ts
+++ src/common/cmd/thread.ts
@@ -1,24 +1,26 @@
-import { ChildProcess, spawn } from "child_process";
+import { ChildProcess, exec } from "child_process";
 import * as process from "process";
 import { CommandLineTask } from "./command-line-task.interface";
 
-const messageHandler = (type: string, taskId?: string, value?: any): void => {
+const _messageHandler = (type: string, taskId?: string, value?: any): void => {
 	process.send && process.send({ taskId, type, value });
 };
 
 process.on("message", (task: CommandLineTask) => {
-	const _command: ChildProcess = spawn(task.command, task.args);
-	
-	_command.on("exit", code => messageHandler("exit", task.taskId, code));
 
-	_command.on("error", error => messageHandler("error", task.taskId, error));
+	const _script: string = task.command + " " + task.args.join(" ");
+
+	const _command: ChildProcess = exec(_script, {
+		cwd: task.directory,
+		windowsHide: true
+	});
 
 	if (task.output)
-		_command.stdout?.on("data", chunk => messageHandler("stdout", task.taskId, chunk));
+		_command.stdout?.on("data", chunk => _messageHandler("stdout", task.taskId, chunk));
 
-	_command.stdout?.on("exit", code => messageHandler("exit", task.taskId, code));
+	_command.stdout?.on("exit", code => _messageHandler("exit", task.taskId, code));
 
-	_command.stdout?.on("close", () => messageHandler("close", task.taskId));
+	_command.stdout?.on("close", () => _messageHandler("close", task.taskId));
 
-	_command.stdout?.on("error", error => messageHandler("error", task.taskId, error));
+	_command.stdout?.on("error", error => _messageHandler("error", task.taskId, error));
 });
