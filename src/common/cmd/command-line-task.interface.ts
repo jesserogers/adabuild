48061928933e8160d6caf46827da7a01bd1b3694
Jesse Rogers <jesse.rogers@adaptiva.com>
2022-03-03 15:04:39 -0800

    add optional delay property to CommandLineTask

--- src/common/cmd/command-line-task.interface.ts
+++ src/common/cmd/command-line-task.interface.ts
@@ -3,6 +3,7 @@ import { randomUUID } from "crypto";
 export interface CommandLineTask {
 	args: string[];
 	command: string;
+	delay?: number;
 	directory?: string;
 	output?: boolean;
 	taskId?: string;
@@ -12,6 +13,7 @@ export class CommandLineTask implements CommandLineTask {
 	constructor(task: CommandLineTask) {
 		this.args = task.args;
 		this.command = task.command;
+		this.delay = task.delay || 0;
 		this.directory = task.directory;
 		this.output = task.output;
 		this.taskId = task.taskId || randomUUID();
