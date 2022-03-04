b2c4a0b16d27bdc74ddf5ef64c64bc4a2d5b583d
Jesse Rogers <jesse.rogers@adaptiva.com>
2022-03-03 21:24:36 -0800

    move CLI file system logic to base class
    no use in going through VS Code's library for the extension when I have to write a native implementation anyway

--- src/cli/filesystem/cli-filesystem.service.ts
+++ src/cli/filesystem/cli-filesystem.service.ts
@@ -1,8 +1,5 @@
 import { Inject, Injectable } from "@kuroi/syringe";
-import * as FileSystem from "fs";
-import { BaseFileSystemService, BaseLoggingService, IWatcher } from "../../common";
-import { ChokidarEventListener } from "./chokidar-event-listener.type";
-import { ChokidarService } from "./chokidar.service";
+import { BaseFileSystemService, BaseLoggingService, ChokidarService } from "../../common";
 
 /**
  * @author Jesse Rogers <jesse.rogers@adaptiva.com>
@@ -16,68 +13,14 @@ export class CliFileSystemService extends BaseFileSystemService implements BaseF
 	private _root: string = process.cwd();
 
 	get root(): string {
-		return this._root || "";
+		return this._root;
 	}
 
 	constructor(
-		@Inject(BaseLoggingService) private logging: BaseLoggingService,
-		@Inject(ChokidarService) private chokidar: ChokidarService
+		@Inject(BaseLoggingService) logging: BaseLoggingService,
+		@Inject(ChokidarService) chokidar: ChokidarService
 	) {
-		super();
-	}
-
-	watch(glob: string): IWatcher<ChokidarEventListener> {
-		return this.chokidar.createWatcher(glob);
-	}
-	
-	readFile<T>(path: string, parse: boolean = true): Promise<T> {
-		return new Promise((resolve, reject) => {
-			FileSystem.readFile(path, "utf-8", (err: NodeJS.ErrnoException | null, data: string) => {
-				if (err) {
-					console.error(err);
-					reject(err.message);
-				}
-				try {
-					if (parse)
-						resolve(JSON.parse(data) as T);
-
-					// return file content as string without parsing
-					resolve(<any>data as T);
-				} catch (_err) {
-					reject(_err);
-				}
-			});
-		});
-	}
-	
-	getDirectory(path: string): Promise<any> {
-		throw new Error("Method not implemented.");
-	}
-	
-	copyFile(source: string, destination: string): Promise<boolean> {
-		return this.readFile<any>(source).then(_content =>
-			this.writeFile(destination, _content).then(() => true).catch(_err => {
-				this.logging.error(_err);
-				return false;
-			})
-		).catch(_err => {
-			this.logging.error(_err);
-			return false;
-		});
-	}
-	
-	writeFile(path: string, content: string | Object): Promise<void> {
-		if (typeof content === "object")
-			content = JSON.stringify(content);
-		
-		return new Promise((resolve, reject) => {
-			FileSystem.writeFile(path, content as string, (_err: any) => {
-				if (_err)
-					reject(_err);
-
-				resolve(void 0);
-			});
-		});
+		super(logging, chokidar);
 	}
 
 }
--- src/common/filesystem/base-filesystem-service.ts
+++ src/common/filesystem/base-filesystem-service.ts
@@ -1,14 +1,71 @@
+import * as FileSystem from "fs";
+import { BaseLoggingService } from "../logging";
+import { ChokidarEventListener } from "./chokidar-event-listener.type";
+import { ChokidarService } from "./chokidar.service";
 import { IWatcher } from "./watcher.interface";
 
-export interface BaseFileSystemService {
+export abstract class BaseFileSystemService {
 
-}
-
-export abstract class BaseFileSystemService implements BaseFileSystemService {
 	abstract get root(): string;
-	abstract watch(glob: string): IWatcher<any>;
-	abstract readFile<T>(path: string, parse?: boolean): Promise<T>;
-	abstract getDirectory(path: string): Promise<any>;
-	abstract copyFile(source: string, destination: string): Promise<boolean>;
-	abstract writeFile(path: string, content: string | Object): Promise<void>;
+
+	constructor(
+		protected logging: BaseLoggingService,
+		protected chokidar: ChokidarService
+	) {
+
+	}
+
+	watch(glob: string): IWatcher<ChokidarEventListener> {
+		return this.chokidar.createWatcher(glob);
+	}
+	
+	readFile<T>(path: string, parse: boolean = true): Promise<T> {
+		return new Promise((resolve, reject) => {
+			FileSystem.readFile(path, "utf-8", (err: NodeJS.ErrnoException | null, data: string) => {
+				if (err) {
+					console.error(err);
+					reject(err.message);
+				}
+				try {
+					if (parse)
+						resolve(JSON.parse(data) as T);
+
+					// return file content as string without parsing
+					resolve(<any>data as T);
+				} catch (_err) {
+					reject(_err);
+				}
+			});
+		});
+	}
+	
+	getDirectory(path: string): Promise<any> {
+		throw new Error("Method not implemented.");
+	}
+	
+	copyFile(source: string, destination: string): Promise<boolean> {
+		return this.readFile<any>(source).then(_content =>
+			this.writeFile(destination, _content).then(() => true).catch(_err => {
+				this.logging.error(_err);
+				return false;
+			})
+		).catch(_err => {
+			this.logging.error(_err);
+			return false;
+		});
+	}
+	
+	writeFile(path: string, content: string | Object): Promise<void> {
+		if (typeof content === "object")
+			content = JSON.stringify(content);
+		
+		return new Promise((resolve, reject) => {
+			FileSystem.writeFile(path, content as string, (_err: any) => {
+				if (_err)
+					reject(_err);
+
+				resolve(void 0);
+			});
+		});
+	}
 }
