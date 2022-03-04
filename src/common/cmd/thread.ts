9f224dafcabaf9bac510beebbaa6fc09c3bd4e02
Jesse Rogers <jesse.rogers@adaptiva.com>
2022-03-03 21:21:36 -0800

    send latest stdout/stderr along with exit/error events

--- src/common/cmd/thread.ts
+++ src/common/cmd/thread.ts
@@ -2,9 +2,11 @@ import { exec } from "child_process";
 import * as process from "process";
 import { CommandLineTask } from "./command-line-task.interface";
 
-const _messageHandler = (type: string, taskId?: string, value?: any): void => {
-	process.send && process.send({ taskId, type, value });
+const _messageHandler = (type: string, taskId?: string, value?: any, out?: string): void => {
+	process.send && process.send({ taskId, type, value, out });
 };
+
+let _outMessage: string;
 
 process.on("message", (task: CommandLineTask) => {
 
@@ -13,11 +15,20 @@ process.on("message", (task: CommandLineTask) => {
 	const _options = { cwd: task.directory, windowsHide: true };
 
 	exec(_script, _options, (_error, _stdout, _stderr) => {
-		if (_stdout && task.output)
-			_messageHandler("stdout", task.taskId, _stdout);
+		if (_stdout) {
+			_outMessage = _stderr;
+			if (task.output)
+				_messageHandler("stdout", task.taskId, _stdout);
+		}
 		if (_stderr)
-			_messageHandler("error", task.taskId, _stderr);
+			_outMessage = _stderr;
+
 	}).on("close", _code => {
-		_messageHandler("exit", task.taskId, _code);
+		if ((_code || 0) > 0)
+			_messageHandler("error", task.taskId, _outMessage);
+		else
+			_messageHandler("exit", task.taskId, _code, _outMessage);
+	}).on("error", _error => {
+		_messageHandler("error", task.taskId, _outMessage);
 	});
 });
