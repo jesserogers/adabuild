8e4db0cbf94719d68fbd392a89dd4896b2694bc3
Jesse Rogers <jesse.rogers@adaptiva.com>
2022-03-03 17:44:23 -0800

    move chokidar logic to common directory

--- src/common/filesystem/chokidar.service.ts
+++ src/common/filesystem/chokidar.service.ts
@@ -1,12 +1,8 @@
-import { Injectable } from "@kuroi/syringe";
 import * as Chokidar from "chokidar";
 import { IWatcher } from "../../common";
 import { ChokidarEventListener } from "./chokidar-event-listener.type";
 import { ChokidarWatcher } from "./chokidar-watcher";
 
-@Injectable({
-	scope: "global"
-})
 export class ChokidarService {
 
 	private _watchers: Map<string, IWatcher<ChokidarEventListener>> = new Map();
--- src/common/filesystem/index.ts
+++ src/common/filesystem/index.ts
@@ -1,2 +1,5 @@
 export * from "./base-filesystem-service";
+export * from "./chokidar-event-listener-callback.type";
+export * from "./chokidar-event-listener.type";
+export * from "./chokidar.service";
 export * from "./watcher.interface";
