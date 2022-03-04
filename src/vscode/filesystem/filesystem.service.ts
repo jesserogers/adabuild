1966e8cc24012c3a83275ddb9019c7caa938557e
Jesse Rogers <jesse.rogers@adaptiva.com>
2022-03-03 21:29:16 -0800

    remove VS Code specific logic

--- src/vscode/filesystem/filesystem.service.ts
+++ src/vscode/filesystem/filesystem.service.ts
@@ -1,7 +1,6 @@
 import { Inject, Injectable } from "@kuroi/syringe";
-import { TextDecoder, TextEncoder } from "util";
-import * as vscode from "vscode";
-import { BaseFileSystemService, IWatcher } from "../../common";
+import { workspace } from "vscode";
+import { BaseFileSystemService, BaseLoggingService, ChokidarService } from "../../common";
 
 /**
  * @author Jesse Rogers <jesse.rogers@adaptiva.com>
@@ -10,98 +9,21 @@ import { BaseFileSystemService, IWatcher } from "../../common";
 @Injectable({
 	scope: "global"
 })
-export class FileSystemService extends BaseFileSystemService implements BaseFileSystemService {
+export class FileSystemService extends BaseFileSystemService implements FileSystemService {
 
 	private _root: string = "";
 
 	get root(): string {
-		if (vscode.workspace.workspaceFolders)
-			this._root = vscode.workspace.workspaceFolders[0].uri.fsPath;
-
+		if (workspace.workspaceFolders)
+			this._root = workspace.workspaceFolders[0].uri.fsPath;
 		return this._root || "";
 	}
 
 	constructor(
-		@Inject(TextDecoder) private decoder: TextDecoder,
-		@Inject(TextEncoder) private encoder: TextEncoder
+		@Inject(BaseLoggingService) logging: BaseLoggingService,
+		@Inject(ChokidarService) chokidar: ChokidarService
 	) {
-		super();
-	}
-
-	public watch(glob: string): IWatcher<vscode.Event<vscode.Uri>> {
-		return vscode.workspace.createFileSystemWatcher(glob as vscode.GlobPattern);
-	}
-
-	public readFile<T>(path: string): Promise<T> {
-		return new Promise<T>((resolve, reject) => {
-			vscode.workspace.fs.readFile(vscode.Uri.file(path)).then(
-				_bytes => {
-					if (!_bytes || !_bytes.length)
-						return reject("Unable to locate file at " + path);
-
-					try {
-						return resolve(JSON.parse(this.decoder.decode(_bytes)) as T);
-					} catch (_err) {
-						console.error(_err);
-						return reject("Failed to parse file at " + path);
-					}
-				},
-				_error => {
-					console.error(_error);
-					return reject(_error);
-				}
-			);
-		});
-	}
-
-	public getDirectory(path: string): Promise<[string, vscode.FileType][]> {
-		const _uri: vscode.Uri = vscode.Uri.file(path);
-		return new Promise((resolve, reject) => {
-			vscode.workspace.fs.readDirectory(_uri).then(
-				_directory => resolve(_directory),
-				_error => reject(_error)
-			);
-		});
-	}
-
-	public copyFile(source: string, destination: string): Promise<boolean> {
-		const _sourceUri = vscode.Uri.file(source);
-		const _destinationUri = vscode.Uri.file(destination);
-		return new Promise(resolve => vscode.workspace.fs.copy(_sourceUri, _destinationUri, {
-			overwrite: true
-		}).then(
-			() => {
-				resolve(true);
-			},
-			() => {
-				resolve(false);
-			}
-		));
-	}
-
-	public writeFile(path: string, content: string | Object): Promise<void> {
-		if (!path)
-			return Promise.reject("Invalid path: " + path);
-
-		if (!content)
-			return Promise.reject("No content supplied");
-
-		if (typeof content !== "string")
-			content = JSON.stringify(content);
-
-		return new Promise((resolve, reject) => {
-			vscode.workspace.fs.writeFile(
-				vscode.Uri.file(path),
-				this.encoder.encode(content as string)
-			).then(
-				() => {
-					resolve(void 0);
-				},
-				_err => {
-					reject(_err);
-				}
-			);
-		});
+		super(logging, chokidar);
 	}
 
 }
--- src/vscode/monitor/monitor.service.ts
+++ src/vscode/monitor/monitor.service.ts
@@ -1,6 +1,5 @@
 import { Inject, Injectable, OnDestroy } from "@kuroi/syringe";
-import { Event, Uri } from "vscode";
-import { BaseConfigurationService, BaseFileSystemService, BaseLoggingService, BaseMonitorService, BaseMonitorState, IWatcher } from "../../common";
+import { BaseConfigurationService, BaseFileSystemService, BaseLoggingService, BaseMonitorService, BaseMonitorState } from "../../common";
 
 /**
  * @author Jesse Rogers <jesse.rogers@adaptiva.com>
@@ -11,8 +10,6 @@ import { BaseConfigurationService, BaseFileSystemService, BaseLoggingService, Ba
 })
 export class MonitorService extends BaseMonitorService implements OnDestroy {
 
-	override _watcher!: IWatcher<Event<Uri>>;
-
 	constructor(
 		@Inject(BaseFileSystemService) fileSystem: BaseFileSystemService,
 		@Inject(BaseLoggingService) window: BaseLoggingService,
@@ -20,24 +17,6 @@ export class MonitorService extends BaseMonitorService implements OnDestroy {
 		@Inject(BaseMonitorState) state: BaseMonitorState
 	) {
 		super(fileSystem, window, config, state);
-	}
-
-	public watch(): void {
-		if (this._watcher)
-			this._watcher.dispose();
-	
-		this._watcher = this.fileSystem.watch(this.config.buildConfig.projectsRootGlob);
-		this._changeListener = this._watcher.onDidChange(uri => this._checkChanges(uri));
-		this._createListener = this._watcher.onDidCreate(uri => this._checkChanges(uri));
-		this._deleteListener = this._watcher.onDidDelete(uri => this._checkChanges(uri));
-	}
-
-	private _checkChanges(uri: Uri): void {
-		this.config.buildConfig.projectDefinitions.forEach(_project => {
-			if (uri.path.includes(`/${_project.name}/`)) {
-				this._saveStateOnDebounce(_project.name);
-			}
-		});
 	}
 
 	onDestroy(): void {
