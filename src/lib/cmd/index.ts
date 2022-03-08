e53a7d04918e43039c96b8f7b5e73614d1cc3487
Jesse Rogers <jesse.rogers@adaptiva.com>
2022-03-08 09:36:06 -0800

    rename BaseCommandLineService file

--- src/lib/cmd/index.ts
+++ src/lib/cmd/index.ts
@@ -1,4 +1,4 @@
+export * from "./base-command-line-service";
 export * from "./cli-command.type";
-export * from "./command-line-service";
 export * from "./command-line-task.interface";
 export * from "./thread";
