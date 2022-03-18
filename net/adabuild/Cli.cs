457a450dcf9e213e17709a75fe21214d4ea5379f
Jesse Rogers <jesse.rogers@adaptiva.com>
2022-03-18 15:32:51 -0700

    fill out CLI class

--- net/adabuild/Cli.cs
+++ net/adabuild/Cli.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Threading.Tasks;
 
 namespace adabuild
 {
@@ -7,14 +6,68 @@ namespace adabuild
 	public class Cli
 	{
 
-		private Build.Service BuildService;
+		private Build.Service buildService;
 
-		private Monitor.Service MonitorService;
+		private Monitor.Service monitorService;
 
-		public Cli(Build.Service _buildService, Monitor.Service _monitorService)
+		public Cli(ref Build.Service _buildService, ref Monitor.Service _monitorService)
 		{
-			BuildService = _buildService;
-			MonitorService = _monitorService;
+			buildService = _buildService;
+			monitorService = _monitorService;
+		}
+
+		public void Start()
+		{
+			monitorService.Start();
+			Run();
+		}
+
+		private void Run()
+		{
+			string _input = Prompt();
+			string[] _args = _input.Split(" ");
+			if (_args.Length == 0)
+			{
+				Console.Error.WriteLine("Invalid argument list");
+			}
+			string _command = _args[0].Trim();
+			
+			switch (_args[0])
+			{
+				case "build":
+				{
+					string _project = _args[1];
+					if (_project == null || _project.Length < 1)
+					{
+						Console.Error.WriteLine("Please provide a valid project name");
+						break;
+					}
+					buildService.Build(_project).GetAwaiter().GetResult();
+					// @todo: handle --incremental flag
+					break;
+				}
+
+				case "reset":
+				{
+					monitorService.State.Clear();
+					monitorService.State.Save().GetAwaiter().GetResult();
+					break;
+				}
+
+				case "cls":
+				{
+					Console.Clear();
+					break;
+				}
+			}
+
+			Run();
+		}
+
+		private string Prompt()
+		{
+			Console.Write("adabuild > ");
+			return Console.ReadLine();
 		}
 
 	}
