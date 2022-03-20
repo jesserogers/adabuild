803bb9e1026caac247de87f77904fefef06c2c4d
Jesse Rogers <jesse.rogers@adaptiva.com>
2022-03-20 14:38:57 -0700

    exit conditions + terminal mgmt + copy tsconfig commands

--- src/vscode/app/adabuild.ts
+++ src/vscode/app/adabuild.ts
@@ -6,6 +6,17 @@ export class adabuild {
 
 	private _projectName: string = "";
 
+	private _projectRoot!: string;
+
+	private _running: boolean = false;
+
+	private get root(): string {
+		if (vscode.workspace?.workspaceFolders && vscode.workspace?.workspaceFolders[0])
+			return vscode.workspace.workspaceFolders[0].uri.path;
+
+		return "";
+	}
+
 	constructor() {
 		this._findExistingTerminal();
 	}
@@ -17,22 +28,48 @@ export class adabuild {
 			vscode.commands.registerCommand("adabuild.start", () => this.start()),
 			vscode.commands.registerCommand("adabuild.build", () => this.build()),
 			vscode.commands.registerCommand("adabuild.reset", () => this.reset()),
+			vscode.commands.registerCommand("adabuild.copytsconfigdev", () => this.copyTsConfig("dev")),
+			vscode.commands.registerCommand("adabuild.copytsconfigprod", () => this.copyTsConfig("prod"))
 		];
 	}
 
 	public run(): void {
+		if (this._running)
+			return;
+
 		this._execute("adabuild run");
+		this._running = true;
 	}
 
 	public stop(): void {
+		if (!this._running) {
+			vscode.window.showErrorMessage("[adabuild] adabuild process not running!");
+			return;
+		}
 		this._execute("stop");
 	}
 
+	public pause(): void {
+		if (!this._running) {
+			vscode.window.showErrorMessage("[adabuild] adabuild process not running!");
+			return;
+		}
+		this._execute("pause");
+	}
+
 	public start(): void {
+		if (this._running) {
+			vscode.window.showInformationMessage("[adabuild] adabuild process already running");
+			return;
+		}
 		this._execute("start");
 	}
 
 	public build(): void {
+		if (!this._running) {
+			vscode.window.showErrorMessage("[adabuild] adabuild process not running!");
+			return;
+		}
 		vscode.window.showInputBox({
 			value: this._projectName,
 			placeHolder: "Enter project name"
@@ -48,15 +85,24 @@ export class adabuild {
 	}
 
 	public reset(): void {
+		if (!this._running) {
+			vscode.window.showErrorMessage("[adabuild] adabuild process not running!");
+			return;
+		}
 		this._execute("reset");
 	}
 
-	private _findExistingTerminal(): void {
-		const _terminal: vscode.Terminal | undefined = vscode.window.terminals.find(_terminal =>
-			_terminal.name === "adabuild");
+	public copyTsConfig(_env: string): void {
+		const _source: vscode.Uri = vscode.Uri.file(`${this.root}/tsconfig.${_env}.json`);
+		const _destination: vscode.Uri = vscode.Uri.file(`${this.root}/tsconfig.json`);
+		vscode.workspace.fs.copy(_source, _destination, { overwrite: true });
+	}
 
-		if (_terminal)
-			this.terminal = _terminal;
+	private _findExistingTerminal(): void {
+		const _terminal: vscode.Terminal | undefined = vscode.window.terminals.find(
+			_terminal => _terminal.name === "adabuild");
+
+		this.terminal = _terminal || null;
 	}
 
 	private _createTerminal(): void {
@@ -67,8 +113,10 @@ export class adabuild {
 		if (!this.terminal) {
 			this._createTerminal();
 			vscode.window.onDidCloseTerminal(_terminal => {
-				if (_terminal.name === "adabuild")
+				if (_terminal.name === "adabuild") {
 					this.terminal = null;
+					this._running = false;
+				}
 			});
 		}
 
