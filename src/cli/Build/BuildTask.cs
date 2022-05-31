56192384336b0110832fcc395cabd9c5d9f2e0c7
Jesse Rogers <jesse.rogers@adaptiva.com>
2022-05-31 16:38:23 -0700

    update BuildTask to use a HashSet of project names

--- src/cli/Build/BuildTask.cs
+++ src/cli/Build/BuildTask.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
+
 namespace adabuild.Build
 {
 
 	public struct BuildRequest
 	{
-		public string project;
+		public HashSet<string> projects;
 		
 		public bool incremental;
 		
